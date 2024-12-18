4cc53906d7d237bf6c97bf1595490aab5718e57b
Soner Taşcı <soner.tasci@gmail.com>
2024-12-18 05:58:38 +0000

    ***

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
-// index.js
-// where your node app starts
+const express = require('express');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+// Routing
+app.get("/", function(req, res) {
+
+  res.sendFile(`${__dirname}/views/index.html`);
+
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/easteregg", function(req, res) {
+  res.json({greeting: "Oh, you've found this! Well, congrats! :p"});
 });
 
+app.get("/api/timestamp", function(req, res) {
 
+  const date = new Date();
 
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+  res.json({
+    "unix": date.valueOf(),
+    "utc": date.toUTCString()
+  });
+
+});
+
+app.get("/api/timestamp/:dateParam", function(req, res) {
+
+  let dateParam = req.params.dateParam;
+
+  if (/^\d{5,}$/.test(dateParam))
+    dateParam = parseInt(dateParam);
+    
+  const date = new Date(dateParam);
+
+  if (date.toString() == "Invalid Date") {
+
+    res.json({
+      "error": "Invalid Date"
+    });
+
+  } else {
+
+    res.json({
+      "unix": date.valueOf(),
+      "utc": date.toUTCString()
+    });
+
+  }
+
+});
+
+// No matching route
+app.use(function(req, res, next) {
+
+  res.status(404).sendFile(`${__dirname}/views/404.html`);
+
+});
+
+// Listening for requests
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
